769a2d75c925b41e35789d747496a92dbc3b8931
pengpan <mwolfer@foxmail.com>
2021-08-07 20:34:09 +0800

    Initial commit

---
+++ src/test/java/com/github/pengpan/HttpRequestTest.java
@@ -0,0 +1,20 @@
+package com.github.pengpan;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HttpRequestTest
+{
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue()
+  {
+    assertTrue( true );
+  }
+}
